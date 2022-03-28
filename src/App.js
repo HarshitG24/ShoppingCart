118e06eb460944370d86873d6381298bf2567444
Harshit Gajjar <hrstgajjar3@gmail.com>
2022-03-27 18:56:32 -0700

    added just the space for cart

--- src/App.js
+++ src/App.js
@@ -4,8 +4,16 @@ import ProductList from "./components/Products/ProductList";
 function App() {
   return (
     <div className="App">
-      <h1>Shop</h1>
-      <ProductList />
+      <div className="page-divide">
+        <div className="page-left">
+          <h1>Shop</h1>
+          <ProductList />
+        </div>
+
+        <div className="page-right">
+          <h1>My Cart</h1>
+        </div>
+      </div>
     </div>
   );
 }
